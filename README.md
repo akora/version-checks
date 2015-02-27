342b25354e27740c8c063956b57ce51c0f3e5cbc
Andras Kora <ak@akora.info>
2015-02-27 20:26:21 +0100

    added comments and README file

---
+++ README.md
@@ -0,0 +1,30 @@
+
+The script iterates through a list of packages and first checks whether they are installed at all or not. If a package is installed then it checks its path and version number.
+
+It then displays the information in columns.
+
+Example output:
+
+``` shell
+********************** Version checks **********************
+python          2.7.6           /usr/bin/python
+perl            5.18.2          /usr/bin/perl
+ruby            2.0.0           /usr/bin/ruby
+php             5.5.14          /usr/bin/php
+bash            3.2.57          /bin/bash
+git             2.2.1           /usr/local/git/bin/git
+java            1.8.0_25        /usr/bin/java
+VBoxManage      4.3.20          /usr/bin/VBoxManage
+vagrant         1.7.2           /usr/bin/vagrant
+packer          0.7.5           /usr/local/packer/packer
+node            ------          not installed
+npm             ------          not installed
+```
+
+This script can be included in your `.profile` or `.bash_profile` to run it at each SSH logins.
+
+#### Tested on
+
+* Mac OS X Yosemite 10.10.2
+* Debian 7.8
+* Ubuntu 14.10
--- version-checks.sh
+++ version-checks.sh
@@ -2,25 +2,27 @@
 
 printf "********************** Version checks **********************\n"
 
+# the below list is case sensitive!
 packages=(python perl ruby php bash git java VBoxManage vagrant packer node npm)
 
 for package in ${packages[@]}; do
-  if command -v $package >/dev/null 2>&1; then
+  if command -v $package >/dev/null 2>&1; then # one way of checking if package is installed or not
     path="$(echo `which $package`)"
+    # I wish the below wasn't necessary...
     case $package in
-      "python")     version="$(echo `python -c 'import sys; print(sys.version[:5])'`)";;
-      "perl")       version="$(perl -v | awk 'NR==2 {print $9}' | cut -c3-8)";;
-      "ruby")       version="$(ruby -v | awk '{print $2}' | cut -c1-5)";;
-      "php")        version="$(echo `php -v` | awk '{print $2}' | cut -c1-6)";;
-      "bash")       version="$(bash --version | awk 'NR==1 {print $4}' | cut -c1-6)";;
-      "git")        version="$(git --version | awk '{print $3}')";;
+      "python")     version="$(echo `$package -c 'import sys; print(sys.version[:5])'`)";;
+      "perl")       version="$($package -v | awk 'NR==2 {print $9}' | cut -c3-8)";;
+      "ruby")       version="$($package -v | awk '{print $2}' | cut -c1-5)";;
+      "php")        version="$(echo `$package -v` | awk '{print $2}' | cut -c1-6)";;
+      "bash")       version="$($package --version | awk 'NR==1 {print $4}' | cut -c1-6)";;
+      "git")        version="$($package --version | awk '{print $3}')";;
       "java")       version="$($package -version 2>&1 |awk 'NR==1{ gsub(/"/,""); print $3 }')";;
-      "VBoxManage") version="$(VBoxManage -v | cut -c1-6)";;
-      "vagrant")    version="$(echo `vagrant version` | awk '{print $3}')";;
-      "packer")     version="$(echo `packer version` | awk '{print $2}' | cut -c2-)";;
+      "VBoxManage") version="$($package -v | cut -c1-6)";;
+      "vagrant")    version="$(echo `$package version` | awk '{print $3}')";;
+      "packer")     version="$(echo `$package version` | awk '{print $2}' | cut -c2-)";;
       *)            version="$(echo `$package -v`)";;
     esac
-    if [[ "$version" =~ "v" ]]; then
+    if [[ "$version" =~ "v" ]]; then # remove the initial 'v' where necessary
       version="$(echo $version | cut -c2-)"
     fi
     printf "%-15s %-15s %-20s\n" $package $version $path
@@ -28,3 +30,6 @@ for package in ${packages[@]}; do
     printf "%-15s %-15s %-20s\n" $package "------" "not installed"
   fi
 done
+
+# TODO: extend it to check the latest stable version of each package
+# Is there such a service out there?
